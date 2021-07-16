33b81fa8a127520e513f75c97ec4cf0fc996f514
戸澤海智 <st37071556@icloud.com>
2021-07-16 21:15:08 +0900

    kcuf

--- Assets/2-2 Asynchronous/Fade/FadeByCoroutin.cs
+++ Assets/2-2 Asynchronous/Fade/FadeByCoroutin.cs
@@ -1,63 +1,44 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 「コルーチン」を使ってフェードアウトを実現するコンポーネント
+/// 適当なオブジェクトに追加して使う
+/// </summary>
 public class FadeByCoroutin : MonoBehaviour
 {
-    [SerializeField] float m_fadeTime = 1;
+    /// <summary>フェード用 Image</summary>
     [SerializeField] Image m_fadeImage = default;
+    /// <summary>フェードアウト完了までにかかる時間（秒）/summary>
+    [SerializeField] float m_fadeTime = 1;
     float m_timer = 0;
-    /// <summary>実行中のコルーチン（実行していない時は null）</summary>
-    Coroutine m_coroutine = null;
-    public void StartPause()
+
+    /// <summary>
+    /// フェードアウトを開始する
+    /// </summary>
+    public void Fade()
     {
-        if (m_coroutine == null)    // 停止している時は
-        {
-            // コルーチンを開始する
-            m_coroutine = StartCoroutine(Fadeout());
-            Fadeout();
-        }
-       
-    }
-    IEnumerator Fadeout ()
-    {
-        StartWorkingRoutine();
-        m_timer += Time.deltaTime;
-        Color c = m_fadeImage.color;
-        c.a = m_timer / m_fadeTime;
-        m_fadeImage.color = c;
-        
-        if (m_timer > m_fadeTime)
-        {
-            // フェード完了
-            OnFadeFinished();
-        }
-        yield return null;
-    }
-    void Start()
-    {
-       
-        
+        Debug.Log("コルーチンによる Fade 開始");
+        StartCoroutine(FadeRoutine());
     }
 
-    IEnumerator StartWorkingRoutine()
+    IEnumerator FadeRoutine()
     {
-        
+        // Image から Color を取得し、時間の進行に合わせたアルファを設定して Image に戻す
+        while (true)
+        {
             m_timer += Time.deltaTime;
-            
-            yield return new WaitForEndOfFrame();   // Update() の処理が終わるまで待つ
-        
-    }
+            Color c = m_fadeImage.color;
+            c.a = m_timer / m_fadeTime;
+            m_fadeImage.color = c;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnFadeFinished()
-    {
-        Debug.Log("Fade 完了");
+            if (m_timer > m_fadeTime)
+            {
+                Debug.Log("コルーチンによる Fade 完了");
+                yield break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
