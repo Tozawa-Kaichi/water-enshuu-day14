38eec01c5d207fa665f6a41561943b90b3e7a48b
戸澤海智 <st37071556@icloud.com>
2021-10-23 23:22:55 +0900

    s

--- Assets/2-2 Asynchronous/Fade/FadeByCoroutine.cs
+++ Assets/2-2 Asynchronous/Fade/FadeByCoroutine.cs
@@ -1,19 +1,17 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-
 /// <summary>
 /// 「コルーチン」を使ってフェードアウトを実現するコンポーネント
 /// 適当なオブジェクトに追加して使う
 /// </summary>
-public class FadeByCoroutin : MonoBehaviour
+public class FadeByCoroutine : MonoBehaviour
 {
     /// <summary>フェード用 Image</summary>
     [SerializeField] Image m_fadeImage = default;
     /// <summary>フェードアウト完了までにかかる時間（秒）/summary>
     [SerializeField] float m_fadeTime = 1;
     float m_timer = 0;
-
     /// <summary>
     /// フェードアウトを開始する
     /// </summary>
@@ -22,7 +20,6 @@ public class FadeByCoroutin : MonoBehaviour
         Debug.Log("コルーチンによる Fade 開始");
         StartCoroutine(FadeRoutine());
     }
-
     IEnumerator FadeRoutine()
     {
         // Image から Color を取得し、時間の進行に合わせたアルファを設定して Image に戻す
@@ -32,7 +29,6 @@ public class FadeByCoroutin : MonoBehaviour
             Color c = m_fadeImage.color;
             c.a = m_timer / m_fadeTime;
             m_fadeImage.color = c;
-
             if (m_timer > m_fadeTime)
             {
                 Debug.Log("コルーチンによる Fade 完了");
